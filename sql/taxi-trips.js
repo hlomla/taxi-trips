398cbf685df20f815f8ab011bfa0b71c4ddc7d18
hlomla <hlomla.tapuko@gmail.com>
2021-11-22 16:36:56 +0200

    travis badge-1

---
+++ sql/taxi-trips.js
@@ -0,0 +1,58 @@
+module.exports = function TaxiTrips(pool) {
+	
+    async function insertTrips(routes_id, taxi_id){
+        await pool.query('insert into trip(routes_id, taxi_id) values ($1, $2)', [routes_id, taxi_id]);
+    };
+
+    async function totalTripCount(){
+        console.log(tripCount);
+         var tripCount = await pool.query('select * from trip')
+         return tripCount.rowCount
+    };
+
+    async function findAllRegions(){
+        var allRegions = await pool.query('select name from region')
+        return allRegions.rows
+    };
+
+    async function findTaxisForRegion(){
+        await pool.query('select taxi_id from region')
+    };
+
+    async function findTripsByRegNumber(){
+
+    };
+
+    async function findTripsByRegion(){
+
+    };
+
+    async function findIncomeByRegNumber(){
+
+    };
+
+    async function findTotalIncomePerTaxi(){
+
+    };
+
+    async function findTotalIncome(){
+
+    };
+
+    async function findTotalIncomeByRegion(){
+
+    };
+
+    return {
+    insertTrips,
+    totalTripCount,
+    findAllRegions,
+    findTaxisForRegion,
+    findTripsByRegNumber,
+    findTripsByRegion,
+    findIncomeByRegNumber,
+    findTotalIncomePerTaxi,
+    findTotalIncome,
+    findTotalIncomeByRegion
+    }
+}
