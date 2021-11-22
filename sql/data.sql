7f0b1cb0aa9f9a1192792d94c05c56fbb842e446
hlomla <hlomla.tapuko@gmail.com>
2021-11-22 16:48:20 +0200

    travis badge-4

---
+++ sql/data.sql
@@ -0,0 +1,24 @@
+insert into region(id, name) values (1, 'Durban');
+insert into region(id, name) values (2, 'Cape Town');
+insert into region(id, name) values (3, 'Gauteng');
+
+insert into routes(name, fare) values('Cape Town - Bellville', 14.50);
+insert into routes(name, fare) values('Cape Town - Hout Bay', 10.00);
+insert into routes(name, fare) values('Cape Town - Silversands', 20.00);
+
+insert into routes(name, fare) values('Durban - Molweni', 14.50);
+insert into routes(name, fare) values('Durban - Nongama', 180.00);
+insert into routes(name, fare) values('Durban - Ballito', 25.00);
+
+insert into routes(name, fare) values('Gauteng - Sandton', 18.00);
+insert into routes(name, fare) values('Gauteng - Alexandra', 11.00);
+insert into routes(name, fare) values('Gauteng - Diepsloot', 17.00);
+
+insert into taxi(reg_number) values ('CAA 125789');
+insert into taxi(reg_number) values ('BC 12 DF GP');
+insert into taxi(reg_number) values ('NN 21514');
+
+
+
+
+
